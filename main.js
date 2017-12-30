d550830f5458c4e9da610467677a906a4dc7a258
Ryan Moll <rmmoll23@gmail.com>
2017-12-29 20:54:21 -0700

    results templates on main.js

--- main.js
+++ main.js
@@ -1,41 +1,116 @@
-// breed.list
+// const breedEndpoint = http://api.petfinder.com/breed.list
 const query = {
     key: '93fd0d1782907ee47a856e7d69a61895',
     animal: 'dog',
     format: JSON
 }
 
-// pet.find
+function renderBreedList(breeds) {
+    const breedList = `<option value=${breeds.$t}>Poodle</option>`
+    return breedList;
+}
+
+// const petEndpoint = http://api.petfinder.com/pet.find
 const query = {
     key: '93fd0d1782907ee47a856e7d69a61895',
     breed: getDogBreed(),
     location: getZipCode(),
-    output: 'full',
+    animal: 'dog',
     format: JSON
 }
 
-// shelter.find
+function renderDogSearchResults (pets) {  
+
+    const dogSearchResults = 
+    `<div class="col-4">
+        <div class="profile" id="${pets.id.$t}">
+            <img class="profile-image" src="${pets.media.photos.photo[1].$t}" />
+            <div class="profile-content">
+                <h3>${pets.name.$t}</h3>
+                <p>Age: ${pets.age.$t} Breed: ${pets.breeds.breed.$t}</p>
+                <p>Shelter: getShelterName()</p>
+            </div>
+        </div>
+    </div>`;
+    return dogSearchResults;
+
+}
+
+// const shelterEndpoint = http://api.petfinder.com/shelter.find
 const query = {
     key: '93fd0d1782907ee47a856e7d69a61895',
     location: getZipCode(),
     format: JSON 
 }
 
+function renderShelterSearchResults(shelters) {
+
+    const shelterSearchResults = 
+    `<div class="col-4">
+        <div class="profile" id="${shelters.id.$t}">
+            <img class="profile-image" src="https://tf-assets-prod.s3.amazonaws.com/tf-curric/WEB-DEV-001/2.6.3_challenge_responsive_layout/rey_square.png" />
+            <div class="profile-content">
+                <h3>${shelters.name.$t}</h3>
+                <p>${shelters.address1.$t}</p>
+            </div>
+        </div>
+    </div>`;
+    return shelterSearchResults;
+
+}
+
 
 function getDogBreed() {
-
+    $('.petSearch').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('#breedList');
+        const breed = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        return breed;
+    });
 }
 
 function getZipCode() {
+    $('.petSearch').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('#zipCode');
+        const zipCode = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        return zipCode;
+    });
+
+    $('.shelterSearch').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('#zipCode');
+        const shelterZipCode = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        return shelterZipCode;
+    });
+}
+
+function getShelterName() {
 
 }
 
-<div class="col-4">
-    <div class="profile">
-        <img class="profile-image" src="https://tf-assets-prod.s3.amazonaws.com/tf-curric/WEB-DEV-001/2.6.3_challenge_responsive_layout/rey_square.png" />
-        <div class="profile-content">
-            <h3>Rey</h3>
-            <p>Protagonist, from Jakku</p>
-        </div>
-    </div>
-</div>
+function displayBreedList () {
+  const breedList = data.items.map((item, index) => renderBreedList(item,index));
+    
+    $("#breedList").html(breedList);
+
+}
+
+function displayDogSearchResults() {
+    const dogSearchResults = data.items.map((item, index) => renderDogSearchResults(item,index));
+    
+    $(".dogSearchResults").html(dogSearchResults);
+}
+
+function displayShelterSearchResults() {
+    const shelterSearchResults = data.items.map((item, index) => renderShelterSearchResults(item,index));
+    
+    $(".shelterSearchResults").html(shelterSearchResults);
+
+}
